4a9ed916453557e8dbc2ea5787b73e04679956ea
axel <axel.mastroianni@incloudteam.com>
2023-04-17 13:51:20 +0200

    Till lecture 48

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground") {
-            Debug.Log("Ouch!");
+            crashEffect.Play();
+            Invoke("ReloadScene", 0.2f);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Rock") {
+            crashEffect.Play();
+            Invoke("ReloadScene", 0.2f);
+        }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene("Level1");
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
-            Debug.Log("Finished");
+            finishEffect.Play();
+            Invoke("ReloadScene", 0.5f);
         }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene("Level1");
     }
 }
